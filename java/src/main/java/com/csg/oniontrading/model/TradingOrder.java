42d959a37218e2f8267926f9c98bd90f3a8b4ba7
Marcin <niewiemek@gmail.com>
2017-01-24 12:13:48 +0100

    Trading order types

--- java/src/main/java/com/csg/oniontrading/model/TradingOrder.java
+++ java/src/main/java/com/csg/oniontrading/model/TradingOrder.java
@@ -1,5 +1,7 @@
 package com.csg.oniontrading.model;
 
+import org.joda.time.Interval;
+
 import java.util.UUID;
 
 public class TradingOrder {
@@ -13,5 +15,42 @@ public class TradingOrder {
 
     public String getOrderId() {
         return orderId;
+    }
+
+    public static class ForwardOrder extends  TradingOrder {
+
+        public Pair getPair() {
+            return pair;
+        }
+
+        public void setPair(Pair pair) {
+            this.pair = pair;
+        }
+
+        public Integer getNominal() {
+            return nominal;
+        }
+
+        public void setNominal(Integer nominal) {
+            this.nominal = nominal;
+        }
+
+        public Interval getTenor() {
+            return tenor;
+        }
+
+        public void setTenor(Interval tenor) {
+            this.tenor = tenor;
+        }
+
+        private Pair pair;
+        private Integer nominal;
+        private Interval tenor;
+
+        public  enum Pair {
+            EURUSD,
+            EURCHF,
+            USDCHF;
+        }
     }
 }
